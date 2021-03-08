b79c9ae0f740dcf413b7245df56a93a2af56a479
Al_chemick <alex2015mig@gmail.com>
2021-03-08 19:20:27 +0200

    feat: realize task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min (array = []) {
+    return array.length && array.reduce((current, min) => (
+        current < min ? current : min
+    ));
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max (array = []) {
+    return array.length && array.reduce((current, max) => (
+        current > max ? current : max
+    ));
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg (array = []) {
+    return array.length && array.reduce((current, acum) => (
+      acum + current
+    )) / array.length;
 }
